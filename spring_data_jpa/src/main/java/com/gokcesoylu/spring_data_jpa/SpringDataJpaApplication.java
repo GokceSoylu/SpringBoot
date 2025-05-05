959a8466b623766a5540cf22b59ef7457e48d23b
GokceSoylu <gokcesoylu24@gmail.com>
2025-05-05 22:54:48 +0300

    yeni proje eklendi

--- spring_data_jpa/src/main/java/com/gokcesoylu/spring_data_jpa/SpringDataJpaApplication.java
+++ spring_data_jpa/src/main/java/com/gokcesoylu/spring_data_jpa/SpringDataJpaApplication.java
@@ -1,4 +1,4 @@
-package com.gokcesoylu.starter;
+package com.gokcesoylu.spring_data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
