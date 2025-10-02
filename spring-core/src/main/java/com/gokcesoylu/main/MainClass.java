5487ab4c9e53b48c67120402ee879e9c437f6066
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 14:39:49 +0300

    bean kullanımı

--- spring-core/src/main/java/com/gokcesoylu/main/MainClass.java
+++ spring-core/src/main/java/com/gokcesoylu/main/MainClass.java
@@ -1,8 +1,18 @@
 package com.gokcesoylu.main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.gokcesoylu.entity.User;
+import com.gokcesoylu.config.AppConfig;
+import com.gokcesoylu.services.UserService;
+
 public class MainClass {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserService userService = context.getBean(UserService.class);
 
+        for (User user : userService.getUserList()) {
+            System.out.println(user.getFirstname());
+        }
     }
 }
 // option + shift + o ile kullanılmayan paketleri silebilirsin :))
