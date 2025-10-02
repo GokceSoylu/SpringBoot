95de0df7594157c1943e556aeecf8a3e031a591d
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 14:40:58 +0300

    -

--- spring-core/src/main/java/com/gokcesoylu/main/MainClass.java
+++ spring-core/src/main/java/com/gokcesoylu/main/MainClass.java
@@ -1,13 +1,14 @@
 package com.gokcesoylu.main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
 import com.gokcesoylu.entity.User;
 import com.gokcesoylu.config.AppConfig;
 import com.gokcesoylu.services.UserService;
 
 public class MainClass {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         UserService userService = context.getBean(UserService.class);
 
         for (User user : userService.getUserList()) {
