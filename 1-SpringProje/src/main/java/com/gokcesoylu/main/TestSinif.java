34dc8c8ecac17661378f03479693d274221f31cb
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 11:56:40 +0300

    java sınıfı çalıştırma

---
+++ 1-SpringProje/src/main/java/com/gokcesoylu/main/TestSinif.java
@@ -0,0 +1,8 @@
+package com.gokcesoylu.main;
+
+public class TestSinif {
+    public static void main(String[] srgs) {
+        System.out.print("Gökçe Soylu");
+    }
+
+}// buradan java üzerinden çalıştırıyoruz
