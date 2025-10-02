43ccf2db2e80f4ea26271cfc2fc9897a8923acb8
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 13:23:52 +0300

    lombok ile getter setter

---
+++ spring-core/src/main/java/com/gokcesoylu/entity/User.java
@@ -0,0 +1,14 @@
+package com.gokcesoylu.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String firstname;
+}
