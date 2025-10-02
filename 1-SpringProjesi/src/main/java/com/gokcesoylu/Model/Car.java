aa95e57438827d4a8a778d3d5393ce2c1889cb50
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 11:38:00 +0300

    updated

--- 1-SpringProjesi/src/main/java/com/gokcesoylu/Model/Car.java
+++
@@ -1,5 +0,0 @@
-package com.gokcesoylu.Model;
-
-public class Car {
-    
-}
--- spring_data_jpa/src/main/java/com/gokcesoylu/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gokcesoylu.repository;
-
-import com.gokcesoylu.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
