c9785c1a7169f7d95687bfbaea065a15745aab33
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 19:59:24 +0300

    rest api project created

---
+++ spring-rest-api-p/src/main/java/com/gokcesoylu/spring_rest_api_p/SpringRestApiPApplication.java
@@ -0,0 +1,13 @@
+package com.gokcesoylu.spring_rest_api_p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiPApplication.class, args);
+	}
+
+}
---
+++ spring-rest-api-p/src/test/java/com/gokcesoylu/spring_rest_api_p/SpringRestApiPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokcesoylu.spring_rest_api_p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
