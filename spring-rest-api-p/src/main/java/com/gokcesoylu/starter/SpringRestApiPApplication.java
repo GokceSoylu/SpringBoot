0105e194ccd8b924dea95b8b2f5ac7a3e2be7c17
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 20:04:27 +0300

    mvc architecture created

--- spring-rest-api-p/src/main/java/com/gokcesoylu/starter/SpringRestApiPApplication.java
+++ spring-rest-api-p/src/main/java/com/gokcesoylu/starter/SpringRestApiPApplication.java
@@ -1,4 +1,4 @@
-package com.gokcesoylu.spring_rest_api_p;
+package com.gokcesoylu.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
