c040c88a6afc4390ce0d05246e43bda370b3c33c
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-02 13:28:40 +0300

    service layer

---
+++ spring-core/src/main/java/com/gokcesoylu/services/UserService.java
@@ -0,0 +1,15 @@
+package com.gokcesoylu.services;
+
+import com.gokcesoylu.entity.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class UserService {
+    private List<User> userList;
+
+}
