da802c8f51aca2b4aab0df4a328f27c7d85f6c91
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-03 17:19:34 +0300

    veri tabanı bağlantısı gerçekleşti

--- spring-data-jpa-p/src/main/java/com/gokcesoylu/starter/SpringDataJpaPApplication.java
+++ spring-data-jpa-p/src/main/java/com/gokcesoylu/starter/SpringDataJpaPApplication.java
@@ -1,4 +1,4 @@
-package com.gokcesoylu.spring_data_jpa_p;
+package com.gokcesoylu.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
