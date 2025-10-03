f7e569eabea13ab2953d49ce5a88872be61bbce2
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-03 15:50:00 +0300

    spring-data-jpa project added

---
+++ spring-data-jpa-P/src/main/java/com/gokcesoylu/spring_data_jpa_P/SpringDataJpaPApplication.java
@@ -0,0 +1,13 @@
+package com.gokcesoylu.spring_data_jpa_P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPApplication.class, args);
+	}
+
+}
