4ebc384c27457c21cd0563480575546c382ccbed
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-03 17:17:24 +0300

    jpa project created

---
+++ spring-data-jpa-p/src/test/java/com/gokcesoylu/spring_data_jpa_p/SpringDataJpaPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokcesoylu.spring_data_jpa_p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
