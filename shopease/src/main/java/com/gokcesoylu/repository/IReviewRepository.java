008dfc5189b18ebc1a5a6e5249ff2019e4dcf680
GokceSoylu <gokcesoylu24@gmail.com>
2025-10-13 19:00:32 +0300

    repo katmanı oluşturuldu

---
+++ shopease/src/main/java/com/gokcesoylu/repository/IReviewRepository.java
@@ -0,0 +1,9 @@
+package com.gokcesoylu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IReviewRepository extends JpaRepository {
+
+}
